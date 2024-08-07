2c4c59166c9665c10c037b4823705fbfe548af02
Dastagiri <shaik.dastagiri@qloron.com>
2024-08-07 10:33:16 +0530

    Test fixes

--- src/main/java/com/apptware/interview/serialization/Adult.java
+++ src/main/java/com/apptware/interview/serialization/Adult.java
@@ -1,28 +1,26 @@
 package com.apptware.interview.serialization;
 
-import java.util.Objects;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
 
+import java.io.*;
+import java.util.Objects;
+
 @Data
 @NoArgsConstructor
-public class Adult {
+public class Adult implements Serializable {
 
+  @Serial
+  private static final long serialVersionUID = 1L;
   private String firstName;
   private String lastName;
   private Integer age;
 
-  /**
-   * This class has a constructor validation that restricts illegal Adult instances which doesn't
-   * work during deserialization. Modify the code in this file so that we can also restrict illegal
-   * Adult instances during deserialization.
-   */
   public Adult(String firstName, String lastName, Integer age) {
     if (StringUtils.isBlank(firstName) || StringUtils.isBlank(lastName)) {
       throw new IllegalArgumentException("Firstname or Lastname CANNOT be blank.");
     }
-
     if (Objects.isNull(age) || age < 18) {
       throw new IllegalArgumentException("Inappropriate Age for an Adult.");
     }
@@ -30,5 +28,23 @@ public class Adult {
     this.firstName = firstName;
     this.lastName = lastName;
     this.age = age;
+  }
+
+  @Serial
+  private void writeObject(ObjectOutputStream oos) throws IOException {
+    oos.defaultWriteObject();
+  }
+
+  @Serial
+  private void readObject(ObjectInputStream ois) throws IOException, ClassNotFoundException {
+    ois.defaultReadObject();
+
+    // Validating fields after deserialization
+    if (StringUtils.isBlank(firstName) || StringUtils.isBlank(lastName)) {
+      throw new IllegalArgumentException("Firstname or Lastname CANNOT be blank.");
+    }
+    if (Objects.isNull(age) || age < 18) {
+      throw new IllegalArgumentException("Inappropriate Age for an Adult.");
+    }
   }
 }
